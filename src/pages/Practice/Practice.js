bfffdb0b2b95cb60bd96da3b99042ee2e154163d
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-10 13:35:24 -0700

    Add Header and Timer components to Practice page

--- src/pages/Practice/Practice.js
+++ src/pages/Practice/Practice.js
@@ -1,8 +1,14 @@
 import './Practice.scss'
+import Header from '../../components/Header/Header'
+import Timer from '../../components/Timer/Timer'
 
 export default function Practice() {
     return (
         <>
+            <Header />
+            <div className="practice">
+                <Timer />
+            </div>
         </>
     )
 }
