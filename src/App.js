d294599420c407bbd046c11b88b81e71aade11d0
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-11 14:59:33 -0700

    Add Solve page route to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing/Landing';
 import Practice from './pages/Practice/Practice';
+import Solve from './pages/Solve/Solve';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/solve" element={<Solve />} />
         <Route path="/practice" element={<Practice />} />
       </Routes>
     </BrowserRouter>
