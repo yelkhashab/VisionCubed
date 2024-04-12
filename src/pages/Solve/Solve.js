4c6dbb9dfeb2e2bc92494c6ea02d279bc85db684
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-12 16:13:11 -0700

    Refactor Solve.js to include solution state and make API call to get solution

--- src/pages/Solve/Solve.js
+++ src/pages/Solve/Solve.js
@@ -2,15 +2,32 @@ import './Solve.scss'
 import Header from '../../components/Header/Header'
 import CaptureFace from '../../components/CaptureFace/CaptureFace'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Solve() {
     const [isCapture, setIsCapture] = useState(false)
     const [isSolve, setIsSolve] = useState(false)
     const [cubeState, setCubeState] = useState([])
+    const [solution, setSolution] = useState([])
 
     const handleCubeStateChange = (newCubeState) => {
         setCubeState(newCubeState);
     }
+
+    useEffect(() => {
+        if (Object.keys(cubeState).length === 6) {
+            const getSolution = async () => {
+                const response = await axios.post('http://localhost:8080/api/solve', cubeState)
+                setSolution(response.data);
+            }
+            getSolution();
+        }
+    }, [cubeState])
+
+    useEffect(() => {
+        console.log(solution);
+        
+    }, [solution])
 
     return (
         <>
@@ -19,7 +36,7 @@ export default function Solve() {
                 <div className="solve__title">
                 </div>
                 <div className="solve__capture">
-                    <CaptureFace onCubeStateUpdate={handleCubeStateChange}/>
+                    <CaptureFace onCubeStateUpdate={handleCubeStateChange} />
                 </div>
             </div>
         </>
