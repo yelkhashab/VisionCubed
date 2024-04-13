4292631cd8bd4ad7dbfbdebd3581444b74070e37
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-12 22:43:18 -0700

    Update Solve.js to include RubiksCube component and solution navigation

--- src/pages/Solve/Solve.js
+++ src/pages/Solve/Solve.js
@@ -4,12 +4,17 @@ import CaptureFace from '../../components/CaptureFace/CaptureFace'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+import RubiksCube from '../../components/RubiksCube/RubiksCube.tsx'
+
 export default function Solve() {
     const [isCapture, setIsCapture] = useState(false)
     const [isSolve, setIsSolve] = useState(false)
     const [cubeState, setCubeState] = useState([])
     const [solution, setSolution] = useState([])
+    const [inverseSolution, setInverseSolution] = useState("")
+    const [myScramble, setMyScramble] = useState("")
     const [net, setNet] = useState([])
+    const [stepIndex, setStepIndex] = useState(0);
 
     const handleCubeStateChange = (newCubeState) => {
         setCubeState(newCubeState);
@@ -22,6 +27,7 @@ export default function Solve() {
                     const response = await axios.post('http://localhost:8080/api/solve', cubeState)
                     console.log(response.data);
                     setSolution(response.data['solution'].split(' '));
+                    setInverseSolution(response.data['inverse'].split(' '));
                     setNet(response.data['net']);
                     setIsCapture(false);
                     setIsSolve(true);
@@ -36,37 +42,65 @@ export default function Solve() {
 
     useEffect(() => {
         console.log(solution);
+        console.log(inverseSolution);
     }, [solution])
+
+    useEffect(() => {
+        if (inverseSolution.length > 0) {
+            setMyScramble(inverseSolution.slice(stepIndex).join(' '));
+        }
+    }, [stepIndex, inverseSolution]);
+
+    const handleNext = () => {
+        if (stepIndex < solution.length) {
+            setStepIndex(stepIndex + 1);
+        }
+    };
+
+    const handlePrev = () => {
+        if (stepIndex > 0) {
+            setStepIndex(stepIndex - 1);
+        }
+    };
 
     return (
         <>
             <Header />
             <div className="solve">
-                <div className="solve__title">
-                    <h1>Solve</h1>
-                    <h2>Scan your cube and get the solution</h2>
-                </div>
-                <div className="solve__button-container">
-                    <button className="solve__button" onClick={() => setIsCapture(!isCapture)}>
-                        {isCapture ? 'Stop' : 'Start'}
-                    </button>
-                </div>
-                {isCapture && (
-                    <div className="solve__capture">
-                        <h3>Please align your cube with the grid</h3>
-                        <CaptureFace onCubeStateUpdate={handleCubeStateChange} />
+                <div className="solve__left">
+                    <div className="solve__title">
+                        <h1>Solve</h1>
+                        <h2>Scan your cube and get the solution</h2>
                     </div>
-                )}
-                {isSolve && (
-                    <div className="solve__solution">
-                        <h2 className='solve__solution-heading'>Solution:</h2>
-                        <ul>
-                            {solution.map((step, index) => (
-                                <li key={index}>{step}</li>
-                            ))}
-                        </ul>
+                    <div className="solve__button-container">
+                        <button className="solve__button" onClick={() => setIsCapture(!isCapture)}>
+                            {isCapture ? 'Stop' : 'Start'}
+                        </button>
                     </div>
-                )}
+                    {isCapture && (
+                        <div className="solve__capture">
+                            <h3>Please align your cube with the grid</h3>
+                            <CaptureFace onCubeStateUpdate={handleCubeStateChange} />
+                        </div>
+                    )}
+                    {isSolve && (
+                        <div className="solve__solution">
+                            <h2 className='solve__solution-heading'>Solution:</h2>
+                            <ul>
+                                {solution.map((step, index) => (
+                                    <li key={index} className={index === stepIndex ? 'current' : ''}>{step}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
+                <div className="solve__right">
+                    <RubiksCube myScramble={myScramble} />
+                    <div className="solve__media">
+                        <button onClick={handlePrev} disabled={stepIndex === 0}>Previous</button>
+                        <button onClick={handleNext} disabled={stepIndex === solution.length}>Next</button>
+                    </div>
+                </div>
             </div>
         </>
     )
