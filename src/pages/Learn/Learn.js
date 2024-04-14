5f2a281d9550b8faddb0f7d4e5716e9baa27d2cf
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-14 16:14:45 -0700

    Add script injection for RoofpigCube in Learn.js

--- src/pages/Learn/Learn.js
+++ src/pages/Learn/Learn.js
@@ -1,6 +1,7 @@
 import './Learn.scss'
 import Header from '../../components/Header/Header'
 import RoofpigCube from '../../components/RoofpigCube/RoofpigCube'
+import { Helmet } from 'react-helmet'
 
 export default function Learn() {
     return (
@@ -8,6 +9,11 @@ export default function Learn() {
             <section className='learn'>
                 <Header />
                 <RoofpigCube state="solve" moves="R U  R' U R U2 R' U"/>
+                <Helmet>
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                    <script src="roofpig_and_three.min.js"></script>
+                </Helmet>
+                
             </section>
         </>
     )
