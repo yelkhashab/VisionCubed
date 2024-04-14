d8c1944083934a133e865318bba4ff70fefa5d8d
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-13 23:35:45 -0700

    Refactor Learn.js and Learn.scss to add RoofpigCube component and update styles

--- src/pages/Learn/Learn.js
+++ src/pages/Learn/Learn.js
@@ -1,20 +1,14 @@
 import './Learn.scss'
 import Header from '../../components/Header/Header'
-import { Helmet } from "react-helmet";
+import RoofpigCube from '../../components/RoofpigCube/RoofpigCube'
 
 export default function Learn() {
     return (
         <>
             <section className='learn'>
                 <Header />
-                <section className='learn__moves'>
-                    <div className='roofpig' data-config="alg=R U R' U R U2 R'"></div>
-                </section>
+                <RoofpigCube />
             </section>
-            <Helmet>
-                <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-                <script src="roofpig_and_three.min.js"></script>
-            </Helmet>
         </>
     )
 }
