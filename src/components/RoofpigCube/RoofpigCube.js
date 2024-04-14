ed34a83b3f5cf1a4a172b68bae2a8e841855dc37
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-14 16:14:21 -0700

    Update RoofpigCube.js and RoofpigCube.scss

--- src/components/RoofpigCube/RoofpigCube.js
+++ src/components/RoofpigCube/RoofpigCube.js
@@ -1,23 +1,50 @@
 import './RoofpigCube.scss'
 import { Helmet } from "react-helmet";
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
-export default function RoofpigCube({moves = "", state}) {
+export default function RoofpigCube({ moves, state }) {
+    const roofpigContainerRef = useRef(null);
+    const [cubeConfig, setCubeConfig] = useState("colors=U:w D:y R:r L:o F:g B:b|hover=none")
+    const [load, setLoad] = useState(false)
+
+    useEffect(() => {
+        if (state === "scan") {
+            setCubeConfig("alg=y y y y R> U> x2|colored=*/m|colors=U:w D:y R:o L:r F:b B:g|hover=none")
+            setLoad(true)
+        } else if (state === "solve") {
+            setCubeConfig(`alg=${moves}|colors=U:w D:y R:r L:o F:g B:b|hover=none`)
+            setLoad(true)
+        }
+    }, [state])
+
+    useEffect(() => {
+        if (load && roofpigContainerRef.current) {
+            const div = document.createElement('div');
+            div.className = 'roofpig';
+            div.setAttribute('data-config', cubeConfig);
+            roofpigContainerRef.current.appendChild(div);
+    
+            // Set display to none for the first child after 100ms
+            setTimeout(() => {
+                const firstChild = roofpigContainerRef.current.firstChild;
+                if (firstChild) {
+                    firstChild.style.display = 'none';
+                }
+                
+                const buttons = roofpigContainerRef.current.querySelectorAll('button');
+                buttons.forEach(button => {
+                    button.style.display = 'none';
+                });
+            }, 100);
+        }
+    }, [cubeConfig]);
+
+    if (!cubeConfig) return (<></>)
+
     return (
         <>
-            <section className='roofpig-cube'>
-                <section className='roofpig-cube__moves'>
-                {state === "scan" && (
-                    <div className="roofpig" data-config="alg=y y y y y x x2|colored=*/m|colors=U:w D:y R:o L:r F:b B:g|hover=none"></div>
-                )}
-                {state === "solve" && (
-                    <div className="roofpig" data-config={`alg=${moves}|colors=U:w D:y R:r L:o F:g B:b|hover=none`}></div>
-                )}
-                </section>
-                <Helmet>
-                    <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-                    <script src="roofpig_and_three.min.js"></script>
-                </Helmet>
+            <section className='roofpig-cube' ref={roofpigContainerRef}>
+                <div className='roofpig' data-config={cubeConfig}></div>
             </section>
         </>
     )
