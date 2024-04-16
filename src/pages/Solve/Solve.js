a89c32748e3f344a2ba5f7f869267b02d4f01294
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-16 00:17:06 -0700

    Update padding-top in Solve.scss

--- src/pages/Solve/Solve.js
+++ src/pages/Solve/Solve.js
@@ -1,76 +1,79 @@
 import './Solve.scss'
-import { useEffect, useState } from 'react'
-import { Helmet } from 'react-helmet'
-import { useNavigate } from 'react-router'
+import { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
 
 import Header from '../../components/Header/Header'
 import CaptureFace from '../../components/CaptureFace/CaptureFace'
-import RoofpigCube from '../../components/RoofpigCube/RoofpigCube'
 import RubiksNet from '../../components/RubiksNet/RubiksNet.tsx'
+import AnimCube3 from '../../components/AnimCube3/AnimCube3.js'
 
 export default function Solve() {
     const [isCapture, setIsCapture] = useState(JSON.parse(sessionStorage.getItem('isCapture')) || false);
     const [isSolve, setIsSolve] = useState(JSON.parse(sessionStorage.getItem('isSolve')) || false);
     const [cubeState, setCubeState] = useState(JSON.parse(sessionStorage.getItem('cubeState')) || []);
     const [solution, setSolution] = useState(JSON.parse(sessionStorage.getItem('solution')) || []);
-    const [inverseSolution, setInverseSolution] = useState(JSON.parse(sessionStorage.getItem('inverseSolution')) || "");
-    const [myScramble, setMyScramble] = useState(JSON.parse(sessionStorage.getItem('myScramble')) || "");
-    const [net, setNet] = useState(JSON.parse(sessionStorage.getItem('net')) || []);
     const [stepIndex, setStepIndex] = useState(JSON.parse(sessionStorage.getItem('stepIndex')) || 0);
     const [faceIndex, setFaceIndex] = useState(JSON.parse(sessionStorage.getItem('faceIndex')) || 0);
-    const [buttonsArray, setButtonsArray] = useState(JSON.parse(sessionStorage.getItem('buttonsArray')) || []);
-    const [key, setKey] = useState(JSON.parse(sessionStorage.getItem('key')) || 0);
-    const [roofpigKey, setRoofpigKey] = useState(0);
-    const [reload, setReload] = useState(false);
+    const buttonFuncRef = useRef(null);
 
+    // useEffect(() => {
+    //     sessionStorage.setItem('isCapture', JSON.stringify(isCapture));
+    //     sessionStorage.setItem('isSolve', JSON.stringify(isSolve));
+    //     sessionStorage.setItem('cubeState', JSON.stringify(cubeState));
+    //     sessionStorage.setItem('solution', JSON.stringify(solution));
+    //     sessionStorage.setItem('inverseSolution', JSON.stringify(inverseSolution));
+    //     sessionStorage.setItem('myScramble', JSON.stringify(myScramble));
+    //     sessionStorage.setItem('net', JSON.stringify(net));
+    //     sessionStorage.setItem('stepIndex', JSON.stringify(stepIndex));
+    //     sessionStorage.setItem('faceIndex', JSON.stringify(faceIndex));
+    //     sessionStorage.setItem('buttonsArray', JSON.stringify(buttonsArray));
+    // }, [isCapture, isSolve, cubeState, solution, inverseSolution, myScramble, net, stepIndex, faceIndex, buttonsArray]);
 
-    useEffect(() => {
-        sessionStorage.setItem('isCapture', JSON.stringify(isCapture));
-        sessionStorage.setItem('isSolve', JSON.stringify(isSolve));
-        sessionStorage.setItem('cubeState', JSON.stringify(cubeState));
-        sessionStorage.setItem('solution', JSON.stringify(solution));
-        sessionStorage.setItem('inverseSolution', JSON.stringify(inverseSolution));
-        sessionStorage.setItem('myScramble', JSON.stringify(myScramble));
-        sessionStorage.setItem('net', JSON.stringify(net));
-        sessionStorage.setItem('stepIndex', JSON.stringify(stepIndex));
-        sessionStorage.setItem('faceIndex', JSON.stringify(faceIndex));
-        sessionStorage.setItem('buttonsArray', JSON.stringify(buttonsArray));
-    }, [isCapture, isSolve, cubeState, solution, inverseSolution, myScramble, net, stepIndex, faceIndex, buttonsArray]);
+    // useEffect(() => {
+    //     const storedIsCapture = JSON.parse(sessionStorage.getItem('isCapture'));
+    //     const storedIsSolve = JSON.parse(sessionStorage.getItem('isSolve'));
 
-    useEffect(() => {
-        const storedIsCapture = JSON.parse(sessionStorage.getItem('isCapture'));
-        const storedIsSolve = JSON.parse(sessionStorage.getItem('isSolve'));
-
-        if (!storedIsCapture && isCapture) {
-            setIsSolve(false);
-            setSolution([]);
-            setReload(false);
-            setTimeout(() => {
-                window.location.reload();
-            }, 100);
-        } else if (!storedIsSolve && isSolve) {
-            setIsCapture(false);
-            setReload(false);
-            setTimeout(() => {
-                window.location.reload();
-            }, 100);
-        }
-        setReload(true);
-    }, [isCapture, isSolve]);
+    //     if (!storedIsCapture && isCapture) {
+    //         setIsSolve(false);
+    //         setSolution([]);
+    //         setReload(false);
+    //         setTimeout(() => {
+    //             window.location.reload();
+    //         }, 100);
+    //     } else if (!storedIsSolve && isSolve) {
+    //         setIsCapture(false);
+    //         setReload(false);
+    //         setTimeout(() => {
+    //             window.location.reload();
+    //         }, 100);
+    //     }
+    //     setReload(true);
+    // }, [isCapture, isSolve]);
 
     const handleCubeStateChange = (newCubeState) => {
         setCubeState(newCubeState);
     }
 
-    const handleButtonsLoaded = (buttons) => {
-        setButtonsArray(Array.from(buttons));
+    const handleButtonsLoaded = (buttonFunc) => {
+        console.log(buttonFunc);
+        buttonFuncRef.current = buttonFunc;
+    };
+
+    const handleButtonClick = (btnIndex) => {
+        if (buttonFuncRef.current) {
+            buttonFuncRef.current(btnIndex); // Simulate click on the "step" button (index 5)
+        }
     };
 
     const handleFaceCaptured = (index) => {
         setFaceIndex(index);
-    };
+        handleButtonClick(5)
+        if (faceIndex === 3) {
+            handleButtonClick(5)
+        }
+        console.log(faceIndex);
 
+    };
 
     useEffect(() => {
         if (Object.keys(cubeState).length === 6) {
@@ -78,8 +81,6 @@ export default function Solve() {
                 try {
                     const response = await axios.post('http://localhost:8080/api/solve', cubeState)
                     setSolution(response.data['solution'].split(' '));
-                    setInverseSolution(response.data['inverse'].split(' '));
-                    setNet(response.data['net']);
                     setIsCapture(false);
                     setIsSolve(true);
                 } catch (error) {
@@ -92,54 +93,27 @@ export default function Solve() {
     }, [cubeState])
 
     useEffect(() => {
-        if (inverseSolution.length > 0) {
-            const step = inverseSolution.length - stepIndex;
-            setMyScramble(inverseSolution.slice(0, step).join(' '));
-        }
-    }, [stepIndex, inverseSolution]);
-
-    useEffect(() => {
         if (isCapture) {
             setIsSolve(false);
             setSolution([]);
-            setInverseSolution([]);
-            setNet([]);
             setStepIndex(0);
-            setMyScramble("");
         }
     }, [isCapture])
 
-    useEffect(() => {
-        if (reload) {
-            setTimeout(() => {
-                const button = buttonsArray?.find(button => button.id === 'next-2');
-                for (let i = 0; i < solution.length; i++) {
-                    button?.click();
-                }
-            }, 200)
-            setReload(false);
-        }
-    }, [isSolve])
-
     const handleNext = () => {
-        const button = buttonsArray?.find(button => button.id === 'next-2');
         if (stepIndex < solution.length) {
             setStepIndex(stepIndex + 1);
-            if (button) {
-                button.click();
-            }
         }
     };
 
     const handlePrev = () => {
-        const button = buttonsArray?.find(button => button.id === 'prev-2');
         if (stepIndex > 0) {
             setStepIndex(stepIndex - 1);
-
-            if (button) {
-                button.click();
-            }
         }
+    };
+
+    const handleCapturePhoto = () => {
+        handleNext();
     };
 
     return (
@@ -159,7 +133,7 @@ export default function Solve() {
                     {isCapture && (
                         <div className="solve__capture">
                             <h3>Please align your cube with the grid and scan the faces corrsponding to the center grid color</h3>
-                            <CaptureFace onCubeStateUpdate={handleCubeStateChange} />
+                            <CaptureFace onCubeStateUpdate={handleCubeStateChange} onFaceCaptured={handleFaceCaptured} onCapturePhoto={handleCapturePhoto}/>
                         </div>
                     )}
                     {isSolve && (
@@ -175,30 +149,28 @@ export default function Solve() {
                 </div>
                 <div className="solve__right">
                     {!isSolve && (
-                        <RoofpigCube
-                            key={roofpigKey}
-                            state="scan"
-                            onButtonsLoaded={handleButtonsLoaded}
-                            onFaceCaputured={handleFaceCaptured}
-                        />
+                        <div style={{ width: "400px", height: "400px" }}>
+                            <AnimCube3
+                                params="bgcolor=ffffff&snap=1&buttonbar=0&repeat=0&edit=0&position=0&hint=5&hintborder=1&move=Y Y Y Y X X2&movetext=0&sign=1&counter=0&facelets=****w********y********3********2********1********0****&colors=ff8040ff000000ff000000ff999999aaaa44bb7744994444447744004477"
+                                onButtonsLoaded={handleButtonsLoaded}
+                                onStepClick={handleNext}
+                                onBackClick={handlePrev}
+                            />
+                        </div>
                     )}
                     {isSolve && (
                         <>
-                            <RoofpigCube
-                                key={roofpigKey}
-                                state="solve"
-                                moves={solution.join(' ')}
-                                inverse={inverseSolution.join(' ')}
-                                onButtonsLoaded={handleButtonsLoaded}
-                            />
-                            <RubiksNet myScramble={myScramble} />
+                            <div style={{ width: "400px", height: "400px" }}>
+                                <AnimCube3
+                                    params={`bgcolor=ffffff&snap=1&buttonbar=0&repeat=0&edit=0&scale=2&hint=5&hintborder=1&initrevmove=#&move=${solution.join(' ')}&movetext=0&sign=1&counter=0&facelets=wwwwwwwwwyyyyyyyyynnnnnnnnn333333333000000000111111111&colors=ff8040ff000000ff000000ff999999aaaa44bb7744994444447744004477`}
+                                    onButtonsLoaded={handleButtonsLoaded}
+                                    onStepClick={handleNext}
+                                    onBackClick={handlePrev}
+                                />
+                            </div>
+                            {/* <RubiksNet myScramble={myScramble} /> */}
                         </>
                     )}
-
-                    <div className="solve__media">
-                        <button onClick={handlePrev} disabled={stepIndex === 0}>Previous</button>
-                        <button onClick={handleNext} disabled={stepIndex === solution.length}>Next</button>
-                    </div>
                 </div>
             </div>
         </>
