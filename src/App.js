a825b599082f01798af06cbaee26e5bef7eb9809
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-19 22:49:37 -0700

    Add Learn page and update routing in App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
 import Landing from './pages/Landing/Landing';
-import Practice from './pages/Practice/Practice';
 import Solve from './pages/Solve/Solve';
+import Learn from './pages/Learn/Learn';
+import Practice from './pages/Practice/Practice';
+
 
 export default function App() {
   return (
@@ -13,6 +15,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/solve" element={<Solve />} />
+          <Route path="/learn" element={<Learn />} />
           <Route path="/practice" element={<Practice />} />
         </Routes>
       </BrowserRouter>
