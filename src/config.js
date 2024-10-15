048995f18ec8c465403bfd02f9503dc1cd55bb2a
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-10-14 20:57:55 -0700

    Add backend url to config file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://your-backend-url.herokuapp.com';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://vision-cubed-api-35c4353a750a.herokuapp.com/';
 
 export { API_BASE_URL };
